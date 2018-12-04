7b8cd57742db8f2ba880fbf721ca32704c34a87f
sdoublet <doubletsebastien@sfr.fr>
2018-12-04 15:38:04 +0100

    add a scrollListener in activity for recyclerview

--- app/src/main/java/com/example/doubl/moodtracker/controller/MainActivity.java
+++ app/src/main/java/com/example/doubl/moodtracker/controller/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.doubl.moodtracker.controller;
 
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.LinearSnapHelper;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SnapHelper;
+import android.util.Log;
 import android.view.View;
 
 import com.example.doubl.moodtracker.R;
@@ -20,12 +23,15 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements RecyclerviewAdapter.OnMoodClickedCallBack {
 
+    private MediaPlayer mediaPlayer1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        mediaPlayer1 = MediaPlayer.create(this, R.raw.musique);
         final RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -48,7 +54,23 @@ public class MainActivity extends AppCompatActivity implements RecyclerviewAdapt
         //to fix scroll item
         SnapHelper snapHelper = new LinearSnapHelper();
         snapHelper.attachToRecyclerView(recyclerView);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                Log.d("rv", "onScrollStateChanged() called with: , newState = [" + newState + "]");
+                if (newState==1){
+                    Log.e("state", "you have started scrolling now");
+                }else if (newState == 0){
+                    Log.e("state", "you have stopped scrolling");
+                }
+            }
 
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
 
     }
 
